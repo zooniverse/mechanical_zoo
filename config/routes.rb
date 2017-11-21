3e5e114bef5df5815652f00850add6179f6fa44c
Marten Veldthuis <marten@veldthuis.com>
2017-11-21 15:24:41 +0000

    Automatically submit callback form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resource :session
-  get 'classify/start'
-  get 'classify/callback/:id', to: 'classify#callback'
+
+  resources :classify do
+    collection do
+      get :start
+    end
+
+    member do
+      get :callback
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
